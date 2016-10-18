0c2a780c75da8c7ae62bd6b42cdd28a489c90e97
Daniel Rodriguez <drod2793@gmail.com>
2016-10-18 14:48:23 -0600

    nightwriter working

--- nightwriter.rb
+++ nightwriter.rb
@@ -1,4 +1,21 @@
+require_relative 'translator'
+
 first, second = ARGV
 
 input_file = open(first)
 string = input_file.readline
+
+translator = Translator.new
+braille_array = translator.to_braille(string)
+
+output_file = open(second, 'w')
+line_1 = braille_array[0]
+line_2 = braille_array[1]
+line_3 = braille_array[2]
+
+output_file.write(line_1)
+output_file.write("\n")
+output_file.write(line_2)
+output_file.write("\n")
+output_file.write(line_3)
+output_file.write("\n")
