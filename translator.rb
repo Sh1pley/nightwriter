16f70c1375c3aff5c178f5bd29720e60f0eb8c6f
Daniel Rodriguez <danielrodriguez@Daniels-MBP.domain>
2016-10-17 20:24:24 -0600

    methods for 3 braille lines created

--- translator.rb
+++ translator.rb
@@ -5,11 +5,29 @@ class Translator
     alphabet = Alphabet.new
   end
 
-  def to_braille(string)
-    output_array = []
+  def to_braille_1(string)
+    line_1 = ""
     string.each_char do|character|
-      output_array << alphabet.given_alpha(character)  
+      line_1 << alphabet.given_alpha(character,0)  
     end
-  return output_array  
+  return line_1
   end
+
+  def to_braille_2(string)
+    line_2 = ""
+    string.each_char do|character|
+      line_2 << alphabet.given_alpha(character,1)  
+    end
+  return line_2
+  end
+
+  def to_braille_1(string)
+    line_3 = ""
+    string.each_char do|character|
+      line_3 << alphabet.given_alpha(character,2)  
+    end
+  return line_3
+  end
+
+
 end
