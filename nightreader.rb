f37e4d85e399cfcd8a3c6d9c3169363fe2855141
Daniel Rodriguez <drod2793@gmail.com>
2016-10-18 15:30:45 -0600

    nightreader braille formatted into string

--- nightreader.rb
+++ nightreader.rb
@@ -1,1 +1,19 @@
-work goes here
+require_relative 'translator'
+
+first, second = ARGV
+
+input_file = open(first)
+line_1 = input_file.readline.chomp
+line_2 = input_file.readline.chomp
+line_3 = input_file.readline.chomp
+
+length = line_1.length
+ordered_line = ""
+(0..length-1).step(2) do |index|
+  ordered_line << line_1[index]
+  ordered_line << line_1[index+1]
+  ordered_line << line_2[index]
+  ordered_line << line_2[index+1]
+  ordered_line << line_3[index]
+  ordered_line << line_3[index+1]
+end
