f4710b23c85086be2aaa2e4e25a7f3dd26d7ca4d
Daniel Rodriguez <drod2793@gmail.com>
2016-10-19 18:00:01 -0600

    nw character count

--- lib/nightwriter.rb
+++ lib/nightwriter.rb
@@ -9,12 +9,13 @@ string = input_file.readline
 
 translator = Translator.new
 braille = translator.to_braille(string)
-
+count = 0
 3.times do |i|
   output_file.write(braille[i])
+  count += braille[i].length
   output_file.write("\n")
 end
 
 output_file.close
 
-p "Created '#{ARGV[1]}' from '#{ARGV[0]}'! -----> Contains #{output_text.length} characters.'"
+p "Created '#{ARGV[1]}' from '#{ARGV[0]}'! -----> Contains #{count} characters.'"
